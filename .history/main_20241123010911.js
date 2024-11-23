fd14cd60adb8b5e2f7666c9551697bf144c0cfca
Brian Ho <bh1525@nyu.edu>
2024-11-23 02:26:40 -0600

    demo

---
+++ .history/main_20241123010911.js
@@ -0,0 +1,99 @@
+import './style.css'
+import * as THREE from 'three'
+import { addBoilerPlateMesh, addStandardMesh } from './addMeshes'
+import { addLight } from './addLights'
+import Model from './Model'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { WheelAdaptor } from 'three-story-controls'
+import gsap from 'gsap'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	100
+)
+camera.position.set(0, 0, 5)
+
+//Globals
+const meshes = {}
+const lights = {}
+const controls = new OrbitControls(camera, renderer.domElement)
+let positions = [
+	{ x: 0, y: 0, z: 5 },
+	{ x: 2, y: 0, z: 3 },
+]
+let count = 0
+controls.enablePan = false
+controls.enableZoom = false
+controls.enableRotate = false
+const wheelAdaptor = new WheelAdaptor({ type: 'discrete' })
+wheelAdaptor.connect()
+wheelAdaptor.addEventListener('trigger', () => {
+	if (count >= positions.length - 1) {
+		count = 0
+	} else {
+		count++
+	}
+	gsap.to(controls.target, {
+		x: positions[count].x,
+		y: positions[count].y,
+		z: positions[count].z,
+		ease: 'power3.inOut',
+		duration: 2,
+	})
+	gsap.to(camera.position, {
+		x: positions[count].x,
+		y: positions[count].y,
+		z: positions[count].z,
+		ease: 'power3.inOut',
+		duration: 2,
+	})
+})
+init()
+function init() {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	//meshes
+	meshes.default = addBoilerPlateMesh()
+	meshes.standard = addStandardMesh()
+
+	//lights
+	lights.defaultLight = addLight()
+
+	//changes
+	meshes.default.scale.set(2, 2, 2)
+
+	//scene operations
+	scene.add(meshes.default)
+	scene.add(meshes.standard)
+	scene.add(lights.defaultLight)
+
+	resize()
+	animate()
+}
+
+function resize() {
+	window.addEventListener('resize', () => {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+	})
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+
+	meshes.default.rotation.x += 0.01
+	meshes.default.rotation.z += 0.01
+
+	meshes.standard.rotation.x += 0.01
+	meshes.standard.rotation.z += 0.01
+
+	// meshes.default.scale.x += 0.01
+
+	renderer.render(scene, camera)
+}
